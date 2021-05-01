3fa3fd25c1fd1e891c1411c59e137d5d5462f4f2
Pitometz Arseny <arsensubbotin1@yandex.ru>
2021-05-01 18:50:59 +0300

    Commetns for saving and restarting functions added

--- FileWorkClass.py
+++ FileWorkClass.py
@@ -1,10 +1,11 @@
-#Class for gamer
+#Class for working with the stats file
 class FileWork():
     def __init__(self, LiveCounter, PointCounter, SaveFlag):
         self.LiveCounter =LiveCounter
         self.PointCounter = PointCounter
         self.SaveFlag = SaveFlag
         
+#Opening and copying the data from file        
 f = open('saves.txt', 'r+')
 LiveFromFile = f.readline()
 PointFromFile = f.readline()
@@ -16,5 +17,5 @@ f.close()
 
 PointFromFile = int(PointFromFile)
 LiveFromFile = int(LiveFromFile)
-
+#Creating Stats Class
 Stats=FileWork(LiveFromFile, PointFromFile, 0)
